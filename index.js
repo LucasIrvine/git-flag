130b75d51bb5202b4cf294a04cf1f66d79c8cc1c
Lucas <lucasirvine@gmail.com>
2015-11-16 14:47:02 -0600

    copy and write to hook
    copying the pre-commit hook file and replacing it with a new script,
    all still temp values

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 var shell = require('shelljs');
+//hook file contents (most temp)
+var gitFlagVar = "DEV_MODE";
+var gitFlagValue = true;
+var emptyFileStarter = `#!/bin/sh \n#git-flag variable = ${gitFlagVar} \n${gitFlagVar}=${gitFlagValue} \nif [ "$DEV_MODE" = "true" ]; then \n\techo "Error: you can not commit because ${gitFlagVar} = ${gitFlagValue}" \n\texit 1 \nfi`;
+
 function findGit(hookType) {
 	if(shell.test('-d', '.git')) {
+		console.log('yo we in git!');
 		shell.cd('.git/hooks');
-		if(shell.test('-e', hookType + '.sample')) {
-			console.log(hookType + '.sample is there!');
-		} else if(shell.test('-e', hookType)) {
-			console.log(hookType + '.sample is there!');
+		if(shell.test('-e', hookType)) {
+			shell.cp(hookType, hookType + '.git-flag-copy');
+		} else if(shell.test('-e', hookType + '.sample')) {
+			shell.cp(hookType + '.sample', hookType);
+			var str = shell.cat(hookType);
+			shell.sed('-i', str, emptyFileStarter, hookType);
 		} else {
 			console.log(hookType + '.sample nor ' + hookType + ' exists here apparently');
 		}
 	} else {
-		console.log('nope, go up');
 		shell.cd('../');
 		findGit(hookType);
 	}
